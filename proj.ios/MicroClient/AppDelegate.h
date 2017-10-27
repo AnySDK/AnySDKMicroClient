58cb7f85a3dd6602766b069797210c825c68ddab
magelevin <magelevin@gmail.com>
2017-10-27 18:06:45 +0800

    add ios project

---
+++ proj.ios/MicroClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebDemo
+//
+//  Created by WangChris on 2017/10/25.
+//  Copyright © 2017年 AnySDK@Cocos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ proj.ios/MicroClient/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WebDemo
+//
+//  Created by WangChris on 2017/10/25.
+//  Copyright © 2017年 AnySDK@Cocos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
